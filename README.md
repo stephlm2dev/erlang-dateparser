2f0c34aa2742a0aed3225c210ac2ff3aa525fb7f
Schmidely Stéphane <schmidely.stephane@icloud.com>
2013-05-06 15:17:01 +0200

    Implémentation de "le X mois"

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Erlang-dateparser
 =================
 
 A date parser made in Erlang
+
+***
+Log :
+
+# Lundi 6 Mai 
+- Implémentation de la fonction de vérification de la présence d un élement dans un tuple
+- Implémentation de la fonction qui concerne "le" (ex : "le 5 Juin")
+- Amélioration et correction de bug de la fonction gérant le "dans X jours"
+
--- src/date_parser.erl
+++ src/date_parser.erl
@@ -1,9 +1,9 @@
 -module (date_parser).
--author("Schmidely Stéphane").
+-author("Schmidely Stephane").
 -vsn(0.1).
--compile([debug_info, export_all]).
+%-compile([debug_info, export_all]).
 -import (string, [tokens/2]).
-%-export().
+-export ([parse/1]).
 
 -define(is_num(X),   (X >= $0 andalso X =< $9)).
 -define(is_year(X),  (is_integer(X) andalso X > 31)).
@@ -13,16 +13,16 @@
 -define(GREGORIAN_SECONDS_1970, 62167219200).
 
 -type year()    :: non_neg_integer().
--type month()   :: 1..12.		% .. indicates an interval
+-type month()   :: 1..12.							  % .. indicates an interval
 -type day() 	:: 1..31.
 -type daynum()  :: 1..7.
 -type hour()	:: 0..23.
 -type minute()  :: 0..59.
 -type second()  :: 0..59.
 
--type date()	 :: {day(),month(),year()}. 		 % ie day/month/year
--type time()	 :: {hour(),minute(),second()}.		 % ie hour H minute min second seconds
--type datetime() :: {date(),time()}.				 % ie day/month/year hour H minute min second seconds
+-type date()	 :: {day(),month(),year()}. 		  % ie day/month/year
+-type time()	 :: {hour(),minute(),second()}.		  % ie hour H minute min second seconds
+-type datetime() :: {date(),time()}.				  % ie day/month/year hour H minute min second seconds
 -type now()		 :: {integer(),integer(),integer()}.
 
 % Split the string and analyse each part of the elements
@@ -40,17 +40,14 @@ parse([Year, Month, Day]) ->
 % HIER, AUJOURD'HUI, DEMAIN
 parse(hier) ->
 	Current_date = date(),
-	{Year, Month, Day} = setelement(3, Current_date, element(3,Current_date) - 1),
-	{Day, Month, Year};
+	{Year, Month, Day} = setelement(3, Current_date, element(3,Current_date) - 1);
 		
 parse(aujourdhui) ->	% probleme a regler avec l'apostrophe
-	{Year, Month, Day} = date(),
-	{Day, Month, Year};
+	{Year, Month, Day} = date();
 
 parse(demain) ->
 	Current_date = date(),
-	{Year, Month, Day} = setelement(3, Current_date, element(3,Current_date) + 1),
-	{Day, Month, Year};
+	{Year, Month, Day} = setelement(3, Current_date, element(3,Current_date) + 1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -59,24 +56,27 @@ parse(Periode) when ?is_string(Periode) ->
 	{{Year, Month, Day}, {_,_,_}} = calendar:local_time(),
 	[Mot, Entier, Type] = tokens(Periode, " "),
 	Duree = list_to_integer(Entier),
-	Liste_mois = {"Janvier","Février","Mars","Avril","Mai","Juin","Juillet",
-				 "Août","Septembre","Octobre","Novembre","Décembre"},
+	Liste_mois = {"Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet",
+				 "Aout","Septembre","Octobre","Novembre","Decembre"},
 	% when (?is_day(X) orelse ?is_month(X))
 	case Mot of
-		"dans" when ((Type =:= "jours" orelse Type =:= "jour")) ->	%andalso ?is_day(Duree)
-			Total = 3600*24*Duree + ?GREGORIAN_SECONDS_1970 + 1367591300,	%faire modification temps en secondes!
+		"dans" when (Type =:= "jours" orelse Type =:= "jour") andalso is_integer(Duree) ->	
+			Now_seconds = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
+			Total = 3600*24*Duree + Now_seconds,	%faire modification temps en secondes!
 			calendar:gregorian_seconds_to_datetime(Total);
 
 		"dans" when ( Type =:= "mois") -> 	%andalso ?is_month(Duree)
-			if Duree + Month > 12 -> {Day, (Duree + Month) rem 12, Year};
-			   true -> {Day, Duree + Month, Year}
+			Total = Duree + Month, 
+			if Total > 12 -> {Year + (Total div 12), Total rem 12, Day};
+			   true -> {Year, Duree + Month, Day}
 			end;
 
 		"dans" when ((Type =:= "ans" orelse Type =:= "an")) -> 	%andalso is_integer(Duree)
-			{Day, Month, Year + Duree};
+			{Year + Duree, Month, Day};
 
-		"le"  when (is_in_Tuple(Liste_mois, Type, 0) =/= 0) -> 	%when (is_integer(Duree)) 
-			{Jour, Mois, Annee} = setelement(3, {Year, Month, Day}, Duree);
+		"le" when ?is_day(Duree) -> % si present dans le tuple
+			Numero_mois = is_in_Tuple(Liste_mois, Type),
+			{Annee, Mois, Jour} = setelement(3, {Year, Numero_mois, Day}, Duree);
 		_ -> "Mauvaise phrase"
 	end;
 
@@ -86,7 +86,7 @@ parse(_) ->
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % verifie si l'element est présent dans le tuple et le cas échéant renvoie sa position
-%is_in_Tuple(Tuple, Word) -> is_in_Tuple(Tuple, Word, 0);
+is_in_Tuple(Tuple, Word) -> is_in_Tuple(Tuple, Word, 0).
 
 is_in_Tuple(Tuple, Word, N) when N < 13 ->
 	if 
